ae06b3c63fb4e82a206dba9e96aa2b0306391a71
91880 <91880@DESKTOP-364LPFJ>
2022-10-20 15:57:27 +0530

    prav

--- src/main/java/base_utility/Listner_class.java
+++ src/main/java/base_utility/Listner_class.java
@@ -1,9 +1,27 @@
 package base_utility;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Listner_class {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\91880\\Desktop\\New folder\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver(); 
+		Thread.sleep(5000);
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("asgsgdhdd");
+		Thread.sleep(2000);
+		driver.findElement(By.id("pass")).sendKeys("12354687997");
+		Thread.sleep(1500);
+		driver.findElement(By.xpath("//div[@class='_9lsb _9ls8']")).click();	
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//button[@value='1']")).click();
+		
+		
+		
+		
 
 	}
 
